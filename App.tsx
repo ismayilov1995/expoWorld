a7501385ed4f62c36c713484c92a577b09e4de6b
ismayilov1995 <ismayilov1995@gmail.com>
2020-01-09 00:35:50 +0400

    Using ScrollView

--- App.tsx
+++ App.tsx
@@ -1,48 +1,32 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Image, TextInput, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, Image, Dimensions, ScrollView} from 'react-native';
 import Card from './src/components/Card.js';
 
+const  { width } = Dimensions.get("window"); // Get screen width
 
 export default class App extends Component {
-
-    state = {
-        name: ''
-    };
-
-    _onPressImage = () => {
-        alert('Test');
-    };
-
-    // _onChangeText = text => {
-    //     this.setState({
-    //         name: text,
-    //     });
-    //
-    // };
-
     render() {
-
-        const {name} = this.state;
 
         return (
             <View style={styles.container}>
-                <Text>{name}</Text>
-                <TextInput
-                    value={name}
-                    placeholder='Type anything'
-                    autoCapitalize={"sentences"}
-                    onChangeText={text => {
-                        this.setState({
-                            name: text,
-                        });
-                    }}
-                    style={styles.myInput}/>
-
-                <TextInput
-                    keyboardAppearance= 'dark' // Keyboard Theme
-                    keyboardType={"number-pad"}
-                    secureTextEntry={true}
-                    style={styles.myInput}/>
+                <ScrollView
+                    pagingEnabled={true}
+                    horizontal={true}
+                >
+                    <Text style={styles.title}>1</Text>
+                    <Text style={styles.title}>12</Text>
+                    <Text style={styles.title}>13</Text>
+                    <Text style={styles.title}>14</Text>
+                    <Text style={styles.title}>15</Text>
+                    <Text style={styles.title}>16</Text>
+                    <Text style={styles.title}>17</Text>
+                    <Text style={styles.title}>18</Text>
+                    <Text style={styles.title}>19</Text>
+                    <Text style={styles.title}>10</Text>
+                    <Text style={styles.title}>11</Text>
+                    <Text style={styles.title}>12</Text>
+                    <Text style={styles.title}>13</Text>
+                </ScrollView>
             </View>
         );
     }
@@ -53,17 +37,16 @@ const styles = StyleSheet.create({
         backgroundColor: '#FFEB3B',
         flex: 1,
         flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
     },
-    myInput: {
-        width: '80%',
-        height: 40,
+    title: {
+        backgroundColor: 'orange',
         borderWidth: 2,
-        borderRadius: 10,
-        borderColor: 'white',
-        paddingHorizontal: 10,
-        marginBottom: 20,
+        borderColor: 'yellow',
+        marginVertical: 30,
+        padding: 15,
+        textAlign: 'center',
+        fontSize: 36,
+        width // Set screen width you can use like width: width
 
     },
 
